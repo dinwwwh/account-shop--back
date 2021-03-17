aefdab321a91fed17c0cdf75c480cd702f71ca67
dinhdjj <dinhdjj@gmail.com>
2021-03-17 19:34:35 +0700

    add some App\Actions\.

---
+++ app/Actions/UpdatedAccount.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Account;
+
+class UpdatedAccount
+{
+    static public function make(Account $account)
+    {
+        switch ($account->status_code) {
+            case 'value':
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+        }
+    }
+}
