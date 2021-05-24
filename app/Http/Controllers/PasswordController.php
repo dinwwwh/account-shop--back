d4ac8da3f05bfa44f95ce3e28c19edbe3923c2d4
dinhdjj <dinhdjj@gmail.com>
2021-05-24 19:26:01 +0700

    build: add /sanctum routes for api by ``sanctum``, and add sanctum/* to ``cros`` config

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+
+class PasswordController extends Controller
+{
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? response(['message' => __($status)], 204)
+            : response(['error' => __($status)], 400);
+
+        // return response([], 204);
+    }
+
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status == Password::PASSWORD_RESET
+            ? response(['message' => __($status)], 204)
+            : response(['error' => __($status)], 204);
+
+        // return response([], 204);
+    }
+}
---
+++ routes/sanctum.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| SANCTUM API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "web" middleware group. Enjoy building your API!
+|
+*/
