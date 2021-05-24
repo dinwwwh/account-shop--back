82b4c5acbfc313c12c367abef0f556845cb4dd1e
dinhdjj <dinhdjj@gmail.com>
2021-05-24 20:27:09 +0700

    chore(config/app.php): add ``front_end_url`` config

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'front_end_url' => env('FRONT_END_URL', 'https://front-end.url/'),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
