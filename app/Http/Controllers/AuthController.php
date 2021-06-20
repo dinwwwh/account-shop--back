767a12730642c8e937ca6f0d41a59509f83c34ef
dinhdjj <dinhdjj@gmail.com>
2021-06-20 16:07:42 +0700

    build: add ``UserDetailResource`` and used it in ``AuthController``

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterAuthRequest;
 use App\Http\Requests\LoginAuthRequest;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\UserDetailResource;
 
 class AuthController extends Controller
 {
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (auth()->attempt($credentials, $request->remember)) {
-            return new UserResource(auth()->user());
+            return new UserDetailResource(auth()->user());
         }
 
         return response([
@@ -55,7 +55,7 @@ class AuthController extends Controller
      */
     public function profile()
     {
-        return new UserResource(auth()->user());
+        return new UserDetailResource(auth()->user());
     }
 
     /**
