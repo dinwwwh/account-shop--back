3aeb5fd9ccf2f621f36eadf6fa2bd0ee0f0c4a84
dinhdjj <dinhdjj@gmail.com>
2021-06-29 21:03:54 +0700

    build: add ``StorageFile`` cast used to convert url of storage to full url

---
+++ app/Casts/StorageFile.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+
+class StorageFile implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     * Convert "public/..." to ""http://localhost:8000/storage/..."
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return URL::asset(Storage::url($value));
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, $key, $value, $attributes)
+    {
+        return (string)$value;
+    }
+}
