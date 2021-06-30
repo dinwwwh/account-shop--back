44c9a6fab852225d21989462fe1bf97033a40e43
dinhdjj <dinhdjj@gmail.com>
2021-06-30 09:32:08 +0700

    build: add ``$_with`` & ``$_search`` properties used to with and search all controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Helpers\RequestHelper;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Use as a argument of ->with method to get relationships
+     *
+     * @var array
+     */
+    public array $_with;
+
+    /**
+     * It's word used to search query
+     *
+     * @var string
+     */
+    public string $_search;
+
+    function __construct(Request $request)
+    {
+        $this->_with = RequestHelper::handleWithAttribute($request);
+        $this->_search = (string)$request->_search;
+    }
 }
