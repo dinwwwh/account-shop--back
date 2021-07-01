09d4d1286fb68d10d3db0aa61fe8cc10303394aa
dinhdjj <dinhdjj@gmail.com>
2021-07-01 15:30:08 +0700

    build: change name and add case infinite-depth when equal -1 with method ``convertArrayKeyToCamelCase``

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -12,9 +12,9 @@ class ArrayHelper
      * @param array $arr
      * @return array
      */
-    static function convertToCamelKey(array $arr, int $depth = 1): array
+    static function convertArrayKeyToCamelCase(array $arr, int $depth = 1): array
     {
-        if ($depth <= 0) return $arr;
+        if ($depth == 0) return $arr;
         $result = [];
         foreach ($arr as $key => $value) {
             $result[Str::camel($key)] = is_array($value)
