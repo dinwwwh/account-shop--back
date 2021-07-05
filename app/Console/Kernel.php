584717f7a6913867f9f00448a92e54f8355b7272
dinhdjj <dinhdjj@gmail.com>
2021-07-05 15:08:13 +0700

    build: remove ``DeleteFile`` model and relative ones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,6 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Tasks\DeleteFileTask;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,9 +25,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-
-        $schedule->job(new DeleteFileTask)
-            ->everyFourMinutes();
     }
 
     /**
