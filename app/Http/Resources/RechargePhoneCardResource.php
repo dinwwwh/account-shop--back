3e8b94ac5c9bdde4648060f52badc0fd252cdf9c
dinhdjj <dinhdjj@gmail.com>
2021-08-06 15:57:36 +0700

    build: add abilities of auth in ``RechargePhonecardResource``

--- app/Http/Resources/RechargePhoneCardResource.php
+++ app/Http/Resources/RechargePhoneCardResource.php
@@ -26,6 +26,16 @@ class RechargePhoneCardResource extends Resource
                     ];
                 }
             ),
+
+            // Abilities of auth
+            $this->mergeWhen(
+                auth()->check(),
+                fn () => [
+                    'canReadSensitiveInfos' => auth()->user()->can('readSensitiveInfos', $this->resource),
+                    'canStartApproving' => auth()->user()->can('startApproving', $this->resource),
+                    'canEndApproving' => auth()->user()->can('endApproving', $this->resource),
+                ]
+            )
         ]);
     }
 }
